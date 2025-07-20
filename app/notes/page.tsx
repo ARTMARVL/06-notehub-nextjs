21d012c6f6d49ecae25a82c095d9941eb7b6aac8
ARTMARVL <vladyslav.anishchenko@gmail.com>
2025-07-20 15:53:49 +0200

    2.9

---
+++ app/notes/page.tsx
@@ -0,0 +1,9 @@
+import { fetchNotes } from "@/lib/api";
+import NotesClient from "./Notes.client";
+
+export default async function Notes() {
+  const initialData = await fetchNotes("", 1);
+
+  return <NotesClient initialData={initialData} />;
+}
+
